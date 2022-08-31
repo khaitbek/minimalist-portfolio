0295a1d6df67dbfea902b0036def8ed81b407a15
khaitbek <khaitbek2005@gmail.com>
2022-08-31 15:33:49 +0500

    Finish page

--- dist/index.6895c56f.js
+++ dist/index.6895c56f.js
@@ -2,7 +2,16 @@ const hamburgerButton = document.querySelector(".hamburger");
 const primaryNavigation = document.querySelector("#primaryNavigation");
 hamburgerButton.addEventListener("click", ()=>{
     document.body.toggleAttribute("no-scroll");
-    primaryNavigation.classList.toggle("show");
+    if (primaryNavigation.classList.contains("show")) {
+        primaryNavigation.classList.add("closing");
+        primaryNavigation.addEventListener("animationend", ()=>{
+            primaryNavigation.classList.remove("show", "closing");
+        }, {
+            once: true
+        });
+        return;
+    }
+    primaryNavigation.classList.add("show");
 });
 
 //# sourceMappingURL=index.6895c56f.js.map
--- src/js/nav.js
+++ src/js/nav.js
@@ -3,5 +3,12 @@ const primaryNavigation = document.querySelector("#primaryNavigation")
 
 hamburgerButton.addEventListener("click",()=>{
     document.body.toggleAttribute("no-scroll")
-    primaryNavigation.classList.toggle("show")
+    if(primaryNavigation.classList.contains("show")){
+        primaryNavigation.classList.add("closing");
+        primaryNavigation.addEventListener("animationend",()=>{
+            primaryNavigation.classList.remove("show","closing")
+        },{once:true})
+        return
+    }
+    primaryNavigation.classList.add("show")
 })
